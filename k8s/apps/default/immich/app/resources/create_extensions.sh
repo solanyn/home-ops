25d2923611ff1ca2c8f91a3bfcaa1b4ac1d46d5f
solanyn <14799876+solanyn@users.noreply.github.com>
2024-11-08 21:39:52 +1100

    fix: script

--- k8s/apps/default/immich/app/resources/create_extensions.sh
+++ k8s/apps/default/immich/app/resources/create_extensions.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/env bash
+
+# This is most commonly set to the user 'postgres'
+export INIT_POSTGRES_SUPER_USER=${INIT_POSTGRES_SUPER_USER:-postgres}
+export INIT_POSTGRES_PORT=${INIT_POSTGRES_PORT:-5432}
+
+if [[ -z "${INIT_POSTGRES_HOST}"       ||
+      -z "${INIT_POSTGRES_SUPER_PASS}" ||
+      -z "${INIT_POSTGRES_USER}"       ||
+      -z "${INIT_POSTGRES_PASS}"       ||
+      -z "${INIT_POSTGRES_DBNAME}"
+]]; then
+    printf "\e[1;32m%-6s\e[m\n" "Invalid configuration - missing a required environment variable"
+    [[ -z "${INIT_POSTGRES_HOST}" ]]       && printf "\e[1;32m%-6s\e[m\n" "INIT_POSTGRES_HOST: unset"
+    [[ -z "${INIT_POSTGRES_SUPER_PASS}" ]] && printf "\e[1;32m%-6s\e[m\n" "INIT_POSTGRES_SUPER_PASS: unset"
+    [[ -z "${INIT_POSTGRES_USER}" ]]       && printf "\e[1;32m%-6s\e[m\n" "INIT_POSTGRES_USER: unset"
+    [[ -z "${INIT_POSTGRES_PASS}" ]]       && printf "\e[1;32m%-6s\e[m\n" "INIT_POSTGRES_PASS: unset"
+    [[ -z "${INIT_POSTGRES_DBNAME}" ]]     && printf "\e[1;32m%-6s\e[m\n" "INIT_POSTGRES_DBNAME: unset"
+    exit 1
+fi
+
 # These env are for the psql CLI
 export PGHOST="${INIT_POSTGRES_HOST}"
 export PGUSER="${INIT_POSTGRES_SUPER_USER}"
