f2b3aa6f3b3d804ce6de414e49c040ebd829cb27
Andrew Chen B <andrew.b.chen@ericsson.com>
2024-11-30 22:21:33 +1100

    fix: add from nfs instead

--- k8s/apps/default/calibre/app/resources/init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2154
-set -euo pipefail
-
-curl -fsL -o prideandprejudice.epub https://www.gutenberg.org/ebooks/1342.epub3.images
-calibredb add prideandprejudice.epub --automerge ignore --with-library /config
