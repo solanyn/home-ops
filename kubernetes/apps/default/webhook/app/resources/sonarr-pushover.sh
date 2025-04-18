141502b414dd73a128d612de8fc75ba8619937a8
solanyn <14799876+solanyn@users.noreply.github.com>
2025-04-18 19:50:54 +1000

    chore: update webhook config

--- kubernetes/apps/default/webhook/app/resources/sonarr-pushover.sh
+++ kubernetes/apps/default/webhook/app/resources/sonarr-pushover.sh
@@ -1,57 +1,36 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PUSHOVER_URL=${1:?}
-PAYLOAD=${2:?}
-
-echo "[DEBUG] Payload: ${PAYLOAD}"
-
-function _jq() {
-    jq --raw-output "${1:?}" <<<"${PAYLOAD}"
-}
-
 function notify() {
-    local event="$(_jq '.eventType')"
-    local instance="$(_jq '.instanceName')"
-    local url="$(_jq '.applicationUrl')"
-
-    if [[ "${event}" == "Test" ]]; then
-        printf -v pushover_title "Test Notification"
-        printf -v pushover_msg "Howdy this is a test notification from <b>%s</b>" "${instance}"
-        printf -v pushover_url "%s" "${url}"
-        printf -v pushover_url_title "Open %s" "${instance}"
-        printf -v pushover_priority "%s" "low"
+    if [[ "${SONARR_EVENT_TYPE}" == "Test" ]]; then
+        printf -v PUSHOVER_TITLE "Test Notification"
+        printf -v PUSHOVER_MESSAGE "Howdy this is a test notification"
+        printf -v PUSHOVER_URL "%s" "${SONARR_APPLICATION_URL}"
+        printf -v PUSHOVER_URL_TITLE "View Series"
+        printf -v PUSHOVER_PRIORITY "low"
+    elif [[ "${SONARR_EVENT_TYPE}" == "ManualInteractionRequired" ]]; then
+        printf -v PUSHOVER_TITLE "Episode Requires Manual Interaction"
+        printf -v PUSHOVER_MESSAGE "<b>%s</b><small>\n<b>Client:</b> %s</small>" \
+            "${SONARR_SERIES_TITLE}" \
+            "${SONARR_DOWNLOAD_CLIENT}"
+        printf -v PUSHOVER_URL "%s/activity/queue" "${SONARR_APPLICATION_URL}"
+        printf -v PUSHOVER_URL_TITLE "View Queue"
+        printf -v PUSHOVER_PRIORITY "high"
+    elif [[ "${SONARR_EVENT_TYPE}" == "Download" ]]; then
+        printf -v PUSHOVER_TITLE "Episode Added"
+        printf -v PUSHOVER_MESSAGE "<b>%s (S%02dE%02d)</b><small>\n%s</small><small>\n\n<b>Client:</b> %s</small><small>" \
+            "${SONARR_SERIES_TITLE}" \
+            "${SONARR_EPISODE_SEASON_NUMBER}" \
+            "${SONARR_EPISODE_NUMBER}" \
+            "${SONARR_EPISODE_TITLE}" \
+            "${SONARR_DOWNLOAD_CLIENT}"
+        printf -v PUSHOVER_URL "%s/series/%s" \
+            "${SONARR_APPLICATION_URL}" \
+            "${SONARR_SERIES_TITLE_SLUG}"
+        printf -v PUSHOVER_URL_TITLE "View Series"
+        printf -v PUSHOVER_PRIORITY "low"
     fi
 
-    if [[ "${event}" == "ManualInteractionRequired" ]]; then
-        printf -v pushover_title "%s Import Requires Manual Interaction" "Episode"
-        printf -v pushover_msg "<b>%s (%s)</b><small>\n<b>Client:</b> %s</small>" \
-            "$(_jq '.series.title')" \
-            "$(_jq '.series.year')" \
-            "$(_jq '.downloadClient')"
-        printf -v pushover_url "%s/activity/queue" "${url}"
-        printf -v pushover_url_title "View queue in %s" "${instance}"
-        printf -v pushover_priority "%s" "high"
-    fi
-
-    if [[ "${event}" == "Download" ]]; then
-        printf -v pushover_title "Episode %s" "$([[ "$(_jq '.isUpgrade')" == "true" ]] && echo "Upgraded" || echo "Imported")"
-        printf -v pushover_msg "<b>%s (%s) S%02dE%02d</b><small>\n%s</small><small>\n\n<b>Quality:</b> %s</small><small>\n<b>Size:</b> %s</small><small>\n<b>Client:</b> %s</small><small>\n<b>Indexer:</b> %s</small>" \
-            "$(_jq '.series.title')" \
-            "$(_jq '.series.year')" \
-            "$(_jq '.episodes[0].seasonNumber')" \
-            "$(_jq '.episodes[0].episodeNumber')" \
-            "$(_jq '.episodes[0].title')" \
-            "$(_jq '.episodeFile.quality')" \
-            "$(numfmt --to iec --format "%8.2f" "$(_jq '.episodeFile.size')")" \
-            "$(_jq '.downloadClient')" \
-            "$(_jq '.release.indexer')"
-        printf -v pushover_url "%s/series/%s" \
-            "${url}" \
-            "$(_jq '.series.titleSlug')"
-        printf -v pushover_url_title "View series in %s" "${instance}"
-        printf -v pushover_priority "%s" "low"
-    fi
     apprise -vv --title "${PUSHOVER_TITLE}" --body "${PUSHOVER_MESSAGE}" --input-format html \
         "${SONARR_PUSHOVER_URL}?url=${PUSHOVER_URL}&url_title=${PUSHOVER_URL_TITLE}&priority=${PUSHOVER_PRIORITY}&format=html"
 }
