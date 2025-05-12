e87c9138acdb716afd831291f0614e08d0fe7fcc
solanyn <14799876+solanyn@users.noreply.github.com>
2025-05-12 18:21:43 +1000

    fix: bad copypasta

--- scripts/bootstrap-cluster.sh
+++ scripts/bootstrap-cluster.sh
@@ -1,119 +1,4 @@
-etheus-operator/releases/download/v0.82.1/stripped-down-crds.yaml
-    )
-
-    for crd in "${crds[@]}"; do
-        if kubectl diff --filename "${crd}" &>/dev/null; then
-            log info "CRDs are up-to-date" "crd=${crd}"
-            continue
-        fi
-        if kubectl apply --server-side --filename "${crd}" &>/dev/null; then
-            log info "CRDs applied" "crd=${crd}"
-        else
-            log error "Failed to apply CRDs" "crd=${crd}"
-        fi
-    done
-}
-
-# Resources to be applied before the helmfile charts are installed
-function apply_resources() {
-    log debug "Applying resources"
-
-    local -r resources_file="${ROOT_DIR}/bootstrap/resources.yaml.j2"
-
-    if ! output=$(render_template "${resources_file}") || [[ -z "${output}" ]]; then
-        exit 1
-    fi
-
-    if echo "${output}" | kubectl diff --filename - &>/dev/null; then
-        log info "Resources are up-to-date"
-        return
-    fi
-
-    if echo "${output}" | kubectl apply --server-side --filename - &>/dev/null; then
-        log info "Resources applied"
-    else
-        log error "Failed to apply resources"
-    fi
-}
-
-# Disks in use by rook-ceph must be wiped before Rook is installed
-function wipe_rook_disks() {
-    log debug "Wiping Rook disks"
-
-    # Skip disk wipe if Rook is detected running in the cluster
-    # NOTE: Is there a better way to detect Rook / OSDs?
-    if kubectl --namespace rook-ceph get kustomization rook-ceph &>/dev/null; then
-        log warn "Rook is detected running in the cluster, skipping disk wipe"
-        return
-    fi
-
-    if ! nodes=$(talosctl config info --output json 2>/dev/null | jq --exit-status --raw-output '.nodes | join(" ")') || [[ -z "${nodes}" ]]; then
-        log error "No Talos nodes found"
-    fi
-
-    log debug "Talos nodes discovered" "nodes=${nodes}"
-
-    # Wipe disks on each node that match the ROOK_DISK environment variable
-    for node in ${nodes}; do
-        if ! disks=$(talosctl --nodes "${node}" get disk --output json 2>/dev/null |
-            jq --exit-status --raw-output --slurp '. | map(select(.spec.model == env.ROOK_DISK) | .metadata.id) | join(" ")') || [[ -z "${nodes}" ]]; then
-            log error "No disks found" "node=${node}" "model=${ROOK_DISK}"
-        fi
-
-        log debug "Talos node and disk discovered" "node=${node}" "disks=${disks}"
-
-        # Wipe each disk on the node
-        for disk in ${disks}; do
-            if talosctl --nodes "${node}" wipe disk "${disk}" &>/dev/null; then
-                log info "Disk wiped" "node=${node}" "disk=${disk}"
-            else
-                log error "Failed to wipe disk" "node=${node}" "disk=${disk}"
-            fi
-        done
-    done
-}
-
-# Apply Helm releases using helmfile
-function apply_helm_releases() {
-    log debug "Applying Helm releases with helmfile"
-
-    local -r helmfile_file="${ROOT_DIR}/bootstrap/helmfile.yaml"
-
-    if [[ ! -f "${helmfile_file}" ]]; then
-        log error "File does not exist" "file=${helmfile_file}"
-    fi
-
-    if ! helmfile --file "${helmfile_file}" apply --hide-notes --skip-diff-on-install --suppress-diff --suppress-secrets; then
-        log error "Failed to apply Helm releases"
-    fi
-
-    log info "Helm releases applied successfully"
-}
-
-function main() {
-    check_env KUBECONFIG KUBERNETES_VERSION ROOK_DISK TALOS_VERSION
-    check_cli helmfile jq kubectl kustomize minijinja-cli op talosctl yq
-
-    if ! op whoami --format=json &>/dev/null; then
-        log error "Failed to authenticate with 1Password CLI"
-    fi
-
-    # Bootstrap the Talos node configuration
-    apply_talos_config
-    bootstrap_talos
-    fetch_kubeconfig
-
-    # Apply resources and Helm releases
-    wait_for_nodes
-    wipe_rook_disks
-    apply_crds
-    apply_resources
-    apply_helm_releases
-
-    log info "Congrats! The cluster is bootstrapped and Flux is syncing the Git repository"
-}
-
-main "$@"#!/usr/bin/env bash
+#!/usr/bin/env bash
 set -Eeuo pipefail
 
 source "$(dirname "${0}")/lib/common.sh"
@@ -236,4 +121,119 @@ function apply_crds() {
         # renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
         https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
         # renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
-        https://github.com/prometheus-operator/prom
+        https://github.com/prometheus-operator/prometheus-operator/releases/download/v0.82.1/stripped-down-crds.yaml
+    )
+
+    for crd in "${crds[@]}"; do
+        if kubectl diff --filename "${crd}" &>/dev/null; then
+            log info "CRDs are up-to-date" "crd=${crd}"
+            continue
+        fi
+        if kubectl apply --server-side --filename "${crd}" &>/dev/null; then
+            log info "CRDs applied" "crd=${crd}"
+        else
+            log error "Failed to apply CRDs" "crd=${crd}"
+        fi
+    done
+}
+
+# Resources to be applied before the helmfile charts are installed
+function apply_resources() {
+    log debug "Applying resources"
+
+    local -r resources_file="${ROOT_DIR}/bootstrap/resources.yaml.j2"
+
+    if ! output=$(render_template "${resources_file}") || [[ -z "${output}" ]]; then
+        exit 1
+    fi
+
+    if echo "${output}" | kubectl diff --filename - &>/dev/null; then
+        log info "Resources are up-to-date"
+        return
+    fi
+
+    if echo "${output}" | kubectl apply --server-side --filename - &>/dev/null; then
+        log info "Resources applied"
+    else
+        log error "Failed to apply resources"
+    fi
+}
+
+# Disks in use by rook-ceph must be wiped before Rook is installed
+function wipe_rook_disks() {
+    log debug "Wiping Rook disks"
+
+    # Skip disk wipe if Rook is detected running in the cluster
+    # NOTE: Is there a better way to detect Rook / OSDs?
+    if kubectl --namespace rook-ceph get kustomization rook-ceph &>/dev/null; then
+        log warn "Rook is detected running in the cluster, skipping disk wipe"
+        return
+    fi
+
+    if ! nodes=$(talosctl config info --output json 2>/dev/null | jq --exit-status --raw-output '.nodes | join(" ")') || [[ -z "${nodes}" ]]; then
+        log error "No Talos nodes found"
+    fi
+
+    log debug "Talos nodes discovered" "nodes=${nodes}"
+
+    # Wipe disks on each node that match the ROOK_DISK environment variable
+    for node in ${nodes}; do
+        if ! disks=$(talosctl --nodes "${node}" get disk --output json 2>/dev/null |
+            jq --exit-status --raw-output --slurp '. | map(select(.spec.model == env.ROOK_DISK) | .metadata.id) | join(" ")') || [[ -z "${nodes}" ]]; then
+            log error "No disks found" "node=${node}" "model=${ROOK_DISK}"
+        fi
+
+        log debug "Talos node and disk discovered" "node=${node}" "disks=${disks}"
+
+        # Wipe each disk on the node
+        for disk in ${disks}; do
+            if talosctl --nodes "${node}" wipe disk "${disk}" &>/dev/null; then
+                log info "Disk wiped" "node=${node}" "disk=${disk}"
+            else
+                log error "Failed to wipe disk" "node=${node}" "disk=${disk}"
+            fi
+        done
+    done
+}
+
+# Apply Helm releases using helmfile
+function apply_helm_releases() {
+    log debug "Applying Helm releases with helmfile"
+
+    local -r helmfile_file="${ROOT_DIR}/bootstrap/helmfile.yaml"
+
+    if [[ ! -f "${helmfile_file}" ]]; then
+        log error "File does not exist" "file=${helmfile_file}"
+    fi
+
+    if ! helmfile --file "${helmfile_file}" apply --hide-notes --skip-diff-on-install --suppress-diff --suppress-secrets; then
+        log error "Failed to apply Helm releases"
+    fi
+
+    log info "Helm releases applied successfully"
+}
+
+function main() {
+    check_env KUBECONFIG KUBERNETES_VERSION ROOK_DISK TALOS_VERSION
+    check_cli helmfile jq kubectl kustomize minijinja-cli op talosctl yq
+
+    if ! op whoami --format=json &>/dev/null; then
+        log error "Failed to authenticate with 1Password CLI"
+    fi
+
+    # Bootstrap the Talos node configuration
+    apply_talos_config
+    bootstrap_talos
+    fetch_kubeconfig
+
+    # Apply resources and Helm releases
+    wait_for_nodes
+    wipe_rook_disks
+    apply_crds
+    apply_resources
+    apply_helm_releases
+
+    log info "Congrats! The cluster is bootstrapped and Flux is syncing the Git repository"
+}
+
+main "$@"
