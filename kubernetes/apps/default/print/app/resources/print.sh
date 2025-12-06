9b369c094f805b85da0609fa66bd89cdd56630c7
solanyn <14799876+solanyn@users.noreply.github.com>
2025-12-07 02:02:24 +1100

    chore: convert pdf to RGB image

--- kubernetes/apps/default/print/app/resources/print.sh
+++ kubernetes/apps/default/print/app/resources/print.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-apk add --no-cache cups-client ipptool ghostscript cups-filters
+apk add --no-cache cups-client ipptool cups-filters
 echo "Converting PDF to PWG raster..."
-gs -dNOPAUSE -dBATCH -sDEVICE=pwgraster -sOutputFile=/tmp/testpage.pwg -dProcessColorModel=/DeviceRGB /config/testpage.pdf
+pdftoraster 1 1 1 1 1 /config/testpage.pdf > /tmp/testpage.pwg
 echo "Printing test page..."
 ipptool -tv -f /tmp/testpage.pwg ipp://192.168.1.173/ipp/print /config/print-job.ipp
