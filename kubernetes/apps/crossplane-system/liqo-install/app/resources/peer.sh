4b68a92c15534a0d6bd865191377a686f7f68d6a
solanyn <14799876+solanyn@users.noreply.github.com>
2026-02-02 01:22:35 +1100

    fix(crossplane): download auth plugin in init container
    The google/cloud-sdk image doesn't have curl. Download the
    GKE auth plugin in the init container (alpine/curl) and copy
    it to the main container via shared volume.

--- kubernetes/apps/crossplane-system/liqo-install/app/resources/peer.sh
+++ kubernetes/apps/crossplane-system/liqo-install/app/resources/peer.sh
@@ -3,13 +3,10 @@ set -e
 
 export PATH="/shared:$PATH"
 
-echo "Installing gke-gcloud-auth-plugin..."
-# Download and install the auth plugin manually
-curl -LO https://storage.googleapis.com/cloud-sdk-release/gke-gcloud-auth-plugin-linux-amd64.tar.gz
-tar -xzf gke-gcloud-auth-plugin-linux-amd64.tar.gz
-mv gke-gcloud-auth-plugin /usr/local/bin/
+echo "Setting up gke-gcloud-auth-plugin..."
+# Copy from shared volume and install
+cp /shared/gke-gcloud-auth-plugin /usr/local/bin/
 chmod +x /usr/local/bin/gke-gcloud-auth-plugin
-rm gke-gcloud-auth-plugin-linux-amd64.tar.gz
 
 echo "Authenticating with GCP..."
 gcloud auth activate-service-account --key-file=/etc/gcp/credentials.json
--- kubernetes/apps/crossplane-system/liqo-install/app/resources/setup.sh
+++ kubernetes/apps/crossplane-system/liqo-install/app/resources/setup.sh
@@ -11,4 +11,11 @@ curl -LO https://github.com/liqotech/liqo/releases/download/v0.10.3/liqoctl-linu
 chmod +x liqoctl-linux-amd64
 mv liqoctl-linux-amd64 /shared/liqoctl
 
+echo "Downloading gke-gcloud-auth-plugin..."
+curl -LO https://storage.googleapis.com/cloud-sdk-release/gke-gcloud-auth-plugin-linux-amd64.tar.gz
+tar -xzf gke-gcloud-auth-plugin-linux-amd64.tar.gz
+mv gke-gcloud-auth-plugin /shared/
+chmod +x /shared/gke-gcloud-auth-plugin
+rm gke-gcloud-auth-plugin-linux-amd64.tar.gz
+
 echo "Tools downloaded successfully"
